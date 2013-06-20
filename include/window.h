415805b8844e977309db6be1fdbf29fa5f4d5f85
Johnathan Alvarez <john.lo.alvarez@gmail.com>
2013-06-19 19:48:38 -0700

    getting basic functions for windows setup

--- include/window.h
+++ include/window.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include <GLFW\glfw3.h>
+#include <iostream>
+#include <string>
+
+#define DEFAULT_HEIGHT 480
+#define DEFAULT_WIDTH 640
+
 class Window
 {
-public:
-	Window();
+	public:
+		Window();
+		~Window();
+		void init();
+		void swapBuffer();
+		void update();
+		void clear();
 
-private:
+	private:
+		GLFWwindow* window;
 };
--- src/window.cpp
+++ src/window.cpp
@@ -1,4 +1,36 @@
 #include "window.h"
 
 Window::Window()
-{}
+{
+	if(!glfwInit())
+		std::cout<<"Error: glfw not initilized"<<std::endl;
+	window = glfwCreateWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT, "window", NULL, NULL);
+	if(!window)
+		std::cout<<"Error: glfw window not open"<<std::endl;
+}
+
+Window::~Window()
+{
+	glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void Window::init()
+{
+	glfwMakeContextCurrent(window);
+}
+
+void Window::swapBuffer()
+{
+	glfwSwapBuffers(window);
+}
+
+void Window::update()
+{
+	glfwPollEvents();
+}
+
+void Window::clear()
+{
+	glClear( GL_COLOR_BUFFER_BIT );
+}
