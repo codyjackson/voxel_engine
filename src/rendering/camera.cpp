ceae3d0418e06862352b409294b9850f0379306f
Cody Jackson <sxenog@gmail.com>
2014-01-19 03:08:01 -0800

    I'm now generating my own view matrix since lookAt didn't appear to be working earlier. I may change this back at some point but I'm leaving it for now since it works.
    I also hardcoded a projection matrix. This will need to be generalized.

--- src/rendering/camera.cpp
+++ src/rendering/camera.cpp
@@ -1,6 +1,7 @@
 #include "camera.h"
 
 #include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 Camera::Camera(const glm::vec3& position, const Orientation& orientation)
 :position(position), orientation(orientation)
@@ -8,10 +9,12 @@ Camera::Camera(const glm::vec3& position, const Orientation& orientation)
 
 glm::mat4 Camera::get_view_matrix() const
 {
-	return glm::lookAt(position, orientation.forward(), orientation.up());
+	const glm::mat4 rotation = orientation.get_rotation_matrix();
+	const glm::mat4 translation = glm::translate(position);
+	return glm::inverse(translation * rotation);
 }
 
 glm::mat4 Camera::get_projection_matrix() const
 {
-	return glm::mat4();
+	return glm::perspective(90.0f, 1024.0f/768.0f, 0.1f, 1000.0f);
 }
