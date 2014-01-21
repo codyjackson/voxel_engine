c44ba127be739a3b1baf005aaefedf13096fc938
Cody Jackson <sxenog@gmail.com>
2014-01-20 20:34:37 -0800

    Adding expected transform interface.

---
+++ src/spatial/transform.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "orientation.h"
+
+#include <memory>
+
+class Transform
+{
+public:
+	static std::shared_ptr<Transform> make_transform();
+	static std::shared_ptr<Transform> make_transform(const glm::vec3& position, const Orientation& orientation);
+	static std::shared_ptr<Transform> make_transform(const glm::vec3& position, const Orientation& orientation, std::shared_ptr<Transform> parent);
+
+	void kill_parent();
+	void parent(std::shared_ptr<Transform> parent);
+
+	glm::vec3& position();
+	Orientation& orientation();
+	const glm::vec3& position() const;
+	const Orientation& orientation() const;
+
+	glm::mat4 get_model_matrix() const;
+
+private:
+	Transform();
+	Transform(const glm::vec3& position, const Orientation& orientation, std::shared_ptr<Transform> parent);
+
+	Transform(const Transform& other); //Don't need to define. I'm making this class non copyable. ex: boost::noncopyable
+	Transform& operator=(const Transform&); //Don't need to define.
+
+	glm::vec3& _position;
+	Orientation& _orientation;
+	std::shared_ptr<Transform> _parent;
+};
