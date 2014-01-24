ef12117664a5eda7585a1a19fc129f66155f1ae1
Cody Jackson <sxenog@gmail.com>
2014-01-23 22:22:34 -0800

    Fixed the compiler error in transform. Unfortunately I can't friend the function that creates the Transform object in a portable way so I'm simply using a naked new. This of course is less efficient and exception unsafe but I don't have another particularly good alternative right now.

--- src/spatial/transform.cpp
+++ src/spatial/transform.cpp
@@ -7,11 +7,15 @@ std::shared_ptr<Transform> Transform::make_transform()
 }
 std::shared_ptr<Transform> Transform::make_transform(const glm::vec3& position, const Orientation& orientation)
 {
-	return std::make_shared<Transform>(position, orientation, std::shared_ptr<Transform>());
+	//I would rather use make shared here but unfortunately it seems that friend the function that creates the
+	//object is not portable.
+	return std::shared_ptr<Transform>(new Transform(position, orientation, std::shared_ptr<Transform>()));
 }
 std::shared_ptr<Transform> Transform::make_transform(const glm::vec3& position, const Orientation& orientation, std::shared_ptr<Transform> parent)
 {
-	return std::make_shared<Transform>(position, orientation, parent);
+	//I would rather use make shared here but unfortunately it seems that friend the function that creates the
+	//object is not portable.
+	return std::shared_ptr<Transform>(new Transform(position, orientation, parent));
 }
 
 void Transform::kill_parent()
