64bc75c0518cae41abd36f454a469118531d9dd0
Cody Jackson <sxenog@gmail.com>
2014-07-09 22:00:21 -0700

    Managed to remove more of the cef cruft I was using from the simple app example. Now down to a single class.

--- src/browser/simple_app.cpp
+++ src/browser/simple_app.cpp
@@ -1,15 +1,12 @@
-// Copyright (c) 2013 The Chromium Embedded Framework Authors. All rights
-// reserved. Use of this source code is governed by a BSD-style license that
-// can be found in the LICENSE file.
-
 #include "simple_app.h"
 
-#include <string>
-
+#include "cef/cef_runnable.h"
 #include "simple_handler.h"
 #include "util.h"
-#include "cef/cef_browser.h"
-#include "cef/cef_command_line.h"
+
+#include <algorithm>
+#include <sstream>
+#include <string>
 
 SimpleApp::SimpleApp(const std::string& indexPath, const std::function<void(const CefRenderHandler::RectList&, const void*)>& onPaint)
 :_onPaint(onPaint)
@@ -34,5 +31,62 @@ void SimpleApp::OnContextInitialized() {
   std::string url = "C:/Users/sxenog/Desktop/test.html";
 
   // Create the first browser window.
-  CefBrowserHost::CreateBrowser(window_info, handler.get(), url, browser_settings, NULL);
+  CefBrowserHost::CreateBrowser(window_info, this, url, browser_settings, NULL);
+}
+
+CefRefPtr<CefLoadHandler> SimpleApp::GetLoadHandler()
+{
+	return this;
+}
+
+CefRefPtr<CefRenderHandler> SimpleApp::GetRenderHandler()
+{
+	return this;
+}
+
+void SimpleApp::OnLoadError(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, ErrorCode errorCode, const CefString& errorText, const CefString& failedUrl)
+{
+	REQUIRE_UI_THREAD();
+
+	// Don't display an error for downloaded files.
+	if (errorCode == ERR_ABORTED) {
+		return;
+	}
+
+	// Display a load error message.
+	std::stringstream ss;
+	ss << "<html><body bgcolor=\"white\">"
+		<< "<h2>Failed to load UI</h2> "
+		<< "<h3>Error: "
+		<< std::string(errorText)
+		<< " (" << errorCode
+		<< ")</body></html>";
+	frame->LoadString(ss.str(), failedUrl);
+}
+
+bool SimpleApp::GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect)
+{
+	rect = CefRect(0, 0, 1000, 1000);
+	return true;
+}
+
+void SimpleApp::OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList &dirtyRects, const void *buffer, int width, int height)
+{
+	_onPaint(dirtyRects, buffer);
+}
+
+void SimpleApp::CloseAllBrowsers(bool forceClose)
+{
+	if (!CefCurrentlyOn(TID_UI)) {
+		CefPostTask(TID_UI, NewCefRunnableMethod(this, &SimpleApp::CloseAllBrowsers, forceClose));
+		return;
+	}
+
+	if (_browserList.empty()) {
+		return;
+	}
+
+	std::for_each(std::begin(_browserList), std::end(_browserList), [forceClose](CefRefPtr<CefBrowser> browser){
+		browser->GetHost()->CloseBrowser(forceClose);
+	});
 }
--- src/browser/simple_app.h
+++ src/browser/simple_app.h
@@ -1,22 +1,45 @@
 #pragma once
 
 #include "cef/cef_app.h"
+#include "cef/cef_client.h"
 #include "cef/cef_render_handler.h"
-#include <functional>
 
-class SimpleApp : public CefApp, public CefBrowserProcessHandler 
+#include <functional>
+#include <list>
+
+class SimpleApp : public CefApp, public CefBrowserProcessHandler, public CefClient, public CefLoadHandler, public CefRenderHandler
 {
 public:
 	SimpleApp(const std::string& indexPath, const std::function<void(const CefRenderHandler::RectList&, const void*)>& onPaint);
 
 
 	// CefApp methods:
-	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() OVERRIDE;
+	virtual CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() override;
 
 	// CefBrowserProcessHandler methods:
-	virtual void OnContextInitialized() OVERRIDE;
+	virtual void OnContextInitialized() override;
+
+	// CefClient methods:
+	CefRefPtr<CefLoadHandler> GetLoadHandler() override;
+	CefRefPtr<CefRenderHandler> GetRenderHandler() override;
+
+	// CefLoadHandler methods:
+	void OnLoadError(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, ErrorCode errorCode, const CefString& errorText, const CefString& failedUrl) override;
+
+	// CefRenderHandler methods:
+	bool GetViewRect(CefRefPtr<CefBrowser> browser, CefRect &rect) override;
+	void OnPaint(CefRefPtr<CefBrowser> browser, PaintElementType type, const RectList &dirtyRects, const void *buffer, int width, int height) override;
+
+	// Request that all existing browser windows close.
+	void CloseAllBrowsers(bool forceClose);
 
 private:
 	IMPLEMENT_REFCOUNTING(SimpleApp);
-	std::function<void(const CefRenderHandler::RectList&, const void*)> _onPaint;
+
+	typedef std::list<CefRefPtr<CefBrowser>> BrowserList;
+	BrowserList _browserList;
+
+	int _width;
+	int _height;
+	std::function<void(const RectList&, const void*)> _onPaint;
 };
